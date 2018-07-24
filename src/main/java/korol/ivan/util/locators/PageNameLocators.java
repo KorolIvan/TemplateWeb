eb41b104381a9551ddedc6e124a58397825d47f3
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-24 14:45:11 +0300

    locators class for easy call of any page element

---
+++ src/main/java/korol/ivan/util/locators/PageNameLocators.java
@@ -0,0 +1,10 @@
+package korol.ivan.util.locators;
+
+/**
+ * @author Ivan Korol on 7/24/2018
+ */
+/**
+ * this class will contains locators for special page as a String constants
+ * */
+public class PageNameLocators {
+}
