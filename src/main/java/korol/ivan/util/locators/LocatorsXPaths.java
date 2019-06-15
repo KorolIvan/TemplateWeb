ed95d52e01c5c1e90d280b3c2fab82e014200e5f
Ivan Korol <korol.vanja2008@gmail.com>
2019-06-15 10:24:06 +0200

    universal xpath for button added

---
+++ src/main/java/korol/ivan/util/locators/LocatorsXPaths.java
@@ -0,0 +1,16 @@
+package korol.ivan.util.locators;
+
+/**
+ * @author Ivan Korol on 7/24/2018
+ */
+/**
+ * this class will contains usual locators xPaths
+ * */
+public class LocatorsXPaths {
+    private LocatorsXPaths(){}
+
+    public final static String BUTTON_XPATH = "//input[@type='button'] " +
+            "| //button[@value='%s'] " +
+            "| //div[contains(@Class, 'button')]";
+
+}
